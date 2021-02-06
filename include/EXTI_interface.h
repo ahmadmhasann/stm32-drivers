da2ff262af370c70046217c2044c4a0f7cb0097f
ahmadmhasann <ahmadmhasann@gmail.com>
2021-02-06 02:57:00 +0200

    updated GPIO and RCC drivers

--- include/EXTI_interface.h
+++ include/EXTI_interface.h
@@ -18,24 +18,26 @@ enum {
     EXTI_FALLING_EDGE ,
     EXTI_ON_CHANGE
 };
-enum {
-    EXTI_LINE0  ,
-    EXTI_LINE1  ,
-    EXTI_LINE2  ,
-    EXTI_LINE3  ,
-    EXTI_LINE4  ,
-    EXTI_LINE5  ,
-    EXTI_LINE6  ,
-    EXTI_LINE7  ,
-    EXTI_LINE8  ,
-    EXTI_LINE9  ,
-    EXTI_LINE10 ,
-    EXTI_LINE11 ,
-    EXTI_LINE12 ,
-    EXTI_LINE13 ,
-    EXTI_LINE14 ,
-    EXTI_LINE15
-};
+#define EXTI_RISING_EDGE    0
+#define EXTI_FALLING_EDGE   1
+#define EXTI_ON_CHANGE      2
+
+#define EXTI_LINE0     0
+#define EXTI_LINE1     1
+#define EXTI_LINE2     2
+#define EXTI_LINE3     3
+#define EXTI_LINE4     4
+#define EXTI_LINE5     5
+#define EXTI_LINE6     6
+#define EXTI_LINE7     7
+#define EXTI_LINE8     8
+#define EXTI_LINE9     9
+#define EXTI_LINE10    10
+#define EXTI_LINE11    11
+#define EXTI_LINE12    12
+#define EXTI_LINE13    13
+#define EXTI_LINE14    14
+#define EXTI_LINE15    15
 
 
 #endif
